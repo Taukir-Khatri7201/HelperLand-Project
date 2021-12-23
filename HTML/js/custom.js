b768d0e10842749d305a03c29be6b03c91553eae
Taukirahmed Khatri <khatritaukirahmed@gmail.com>
2021-12-23 19:04:11 +0530

    Completed Become a Provider Page

--- HTML/js/custom.js
+++ HTML/js/custom.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   $(document).scroll(function () {
     var $nav = $(".fixed-top");
+    $navlinks = $("li.nav-item.btn-with-outline");
+
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     if($("nav:first").hasClass("scrolled") || $(window).width() <= 768) {
+      $navlinks.addClass("small-blue-btn");
       document.getElementById("brand-img").setAttribute("src", "assets/logo-small.png");
     } else {
+      $navlinks.removeClass("small-blue-btn");
       document.getElementById("brand-img").setAttribute("src", "assets/logo-large.png");
     }
   });
