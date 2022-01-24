0036d2f3a881a365dcb99dd2ff329355cb055825
Taukirahmed Khatri <khatritaukirahmed@gmail.com>
2022-01-24 22:40:02 +0530

    Completed first integration of HTML

---
+++ Helperland/HelperLand/HelperLand/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelperLand.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult faq()
+        {
+            return View();
+        }
+
+        public IActionResult prices()
+        {
+            return View();
+        }
+    
+        public IActionResult contact()
+        {
+            return View();
+        }
+
+        public IActionResult about()
+        {
+            return View();
+        }
+    }
+}
